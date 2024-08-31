ba32efa4986be4c522fb8f246291a61507654e38
Fabian Uchoa <fabian@forlogic.net>
2024-08-30 22:06:28 -0300

    implementação da cebola sem o js

--- teste.js
+++ teste.js
@@ -7,8 +7,15 @@ document.addEventListener('DOMContentLoaded',function time(){
 });
 
 function trocaImg() {
-    i++
+    
         if(i==4)
             i=0
-     document.getElementById('img1').src = `Imgs/OnionSlices/onion${i}.svg`
+    document.getElementById('img1').src = `Imgs/OnionSlices/onion${i}.svg`
+    document.getElementById(`nav${i}`).style.background = '#000000a1'
+    if(i==0){
+        document.getElementById(`nav3`).style.background = 'white'
+    }else if(i<=3){
+        document.getElementById(`nav${i-1}`).style.background = 'white'
+    }
+    i++
 }

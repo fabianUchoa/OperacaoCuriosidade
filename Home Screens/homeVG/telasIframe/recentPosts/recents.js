9363c98be910ab5388e3391ac81e4cb94a8c1e2f
fabianUchoa <fabian@forlogic.net>
2024-10-07 09:20:38 -0300

    update

--- Home Screens/homeVG/telasIframe/recentPosts/recents.js
+++ Home Screens/homeVG/telasIframe/recentPosts/recents.js
@@ -1,10 +1,11 @@
 function abreModal(optionSelect){
     let iframe = window.parent.document.getElementById('iframeModais');
     iframe.style.display = 'flex';
-    window.parent.document.querySelector('.overlay').style.display = 'flex'
+    window.parent.document.querySelector('.overlay').style.display = 'flex';
     if(optionSelect == 'share'){
         iframe.style.width = '37vw';
         iframe.style.height = '68vh';
+        iframe.src = '/telasHome'
     }
 }
 
